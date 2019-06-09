e83b1b9da5ec9e4ba7f2335be122d736ebbe6aa9
bilalaybar <bilalaybar@gmail.com>
2019-06-09 15:32:17 +0300

    peering module replaced with multi account peering

--- peering/vars.tf
+++ peering/vars.tf
@@ -1,4 +1,32 @@
-variable "vpc" {
-  type = "map"
+variable "enabled" {
+  default     = "true"
+  description = "Set to false to prevent the module from creating or accessing any resources"
+}
 
+variable "name" {
+  description = "Name  (e.g. `app` or `cluster`)"
+  type        = "string"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `a` or `b`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. `{\"BusinessUnit\" = \"XYZ\"`)"
+}
+
+variable "auto_accept" {
+  default     = "true"
+  description = "Automatically accept the peering"
 }
