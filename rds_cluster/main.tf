73c3c2eee7343225937886b827b9e2ff88659f9a
Emir Özbir <emirozbir@kloia.com>
2019-10-31 03:45:31 +0300

    rds_cluster added

---
+++ rds_cluster/main.tf
@@ -0,0 +1,22 @@
+resource "aws_rds_cluster" "rds_cluster" {
+  cluster_identifier = "${var.tag_project}-rds-cluster-${var.profile}"
+  engine             = "${var.engine}"
+  engine_version     = "${var.engine_version}"
+  engine_mode        = "${var.engine_mode}" //"serverless"
+  database_name      = "${var.database_name}" //db
+  master_username    = "${var.master_user}"
+  master_password    = "${var.master_password}"
+  storage_encrypted  = "${var.encrypted}"
+
+  backup_retention_period = 5 # days
+  preferred_backup_window = "03:00-07:00"
+  apply_immediately       = true
+  skip_final_snapshot     = true
+
+  db_subnet_group_name = "${var.rds_subnet_group_id}" //aws_db_subnet_group id
+  vpc_security_group_ids = ["${var.vpc_security_group_id}"] //vpc_security_group_id
+
+  tags = {
+    Name = "${upper(var.tag_project)} RDS Cluster ${var.profile}"
+  }
+}
---
+++ rds_cluster/variable.tf
@@ -0,0 +1,46 @@
+variable "tag_project" {
+  
+}
+
+variable "vpc_security_group_id" {
+  
+}
+
+variable "engine_version" {
+  default = ""
+}
+
+variable "engine" {
+  
+}
+
+variable "engine_mode" {
+  
+}
+
+variable "database_name" {
+  
+}
+
+variable "master_user" {
+  
+}
+
+variable "master_password" {
+  
+}
+
+
+variable "encrypted" {
+    default = true
+}
+
+variable "rds_subnet_group_id" {
+  
+}
+
+
+
+variable "profile" {
+  
+}
