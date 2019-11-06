ff584a576b58cd50d753c2613025b7e3594a6ad2
Emir Özbir <emirozbir@kloia.com>
2019-11-06 18:06:15 +0300

    sqs options added to the mapping module

--- mapping/mapping.tf
+++ mapping/mapping.tf
@@ -12,12 +12,12 @@ resource "null_resource" "queue_mappings" {
 
   triggers {
     queue_name     = "${element(local.queue_names, count.index)}"
-    max_message_size=262144
-    vis_timeout_sec=43200
-    mes_ret_sec=1209600
+    max_message_size="${var.max_message_size}"
+    vis_timeout_sec="${var.vis_timeout_sec}"
+    mes_ret_sec="${var.mes_ret_sec}"
     rec_wait_time_sec=0
-    fifo_queue=false
-    delaysec = 90
+    fifo_queue="${var.fifo_queue_state}"
+    delaysec = "${var.delay_sec}"
   }
 }
 output "queue_mappings" {
@@ -29,12 +29,12 @@ resource "null_resource" "dead_letter_queue_mappings" {
 
   triggers {
     queue_name     = "${element(local.queue_names, count.index)}_dlx"
-    max_message_size=262144
-    vis_timeout_sec=43200
-    mes_ret_sec=1209600
+    max_message_size="${var.max_message_size}"#262144
+    vis_timeout_sec="${var.vis_timeout_sec}"#43200
+    mes_ret_sec="${var.mes_ret_sec}" #1209600
     rec_wait_time_sec=0
-    fifo_queue=false
-    delaysec = 90
+    fifo_queue="${var.fifo_queue_state}"
+    delaysec = "${var.delay_sec}"
 
   }
 }
