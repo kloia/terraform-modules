edd9d2d6dcf561b001419523c5ca61a2158e455c
Emir Özbir <emirozbir@kloia.com>
2019-11-07 00:14:14 +0300

    var files added

---
+++ mapping/variable.tf
@@ -0,0 +1,19 @@
+variable "fifo_queue_state" {
+  default = false
+}
+
+variable "mes_ret_sec" {
+  default = 1209600
+}
+
+variable "vis_timeout_sec" {
+  default = 43200
+}
+
+variable "vis_timeout_sec" {
+  default = 262144
+}
+
+variable "delay_sec" {
+  default = 90
+}
