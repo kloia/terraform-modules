e6d5554e34b182002792369935f44b5dc3692b17
Emir Özbir <emirozbir@kloia.com>
2019-11-06 17:51:24 +0300

    subnet sec groups added

--- vpc-endpoint/main.tf
+++ vpc-endpoint/main.tf
@@ -5,4 +5,10 @@ resource "aws_vpc_endpoint" "vpc_endpoint" {
   tags = {
     Environment = "${var.env_tag}"
   }
+  vpc_endpoint_type = "${var.endpoint_type}"
+
+  security_group_ids = "${var.security_group_ids}"
+
+  subnet_ids          = "${var.subnet_ids}"
+  private_dns_enabled = "${var.private_dns_enabled}"
 }
--- vpc-endpoint/vars.tf
+++ vpc-endpoint/vars.tf
@@ -8,3 +8,23 @@ variable "vpc_id" {
 variable "service_name" {
   default = "com.amazonaws.us-east-1.sqs"
 }
+
+variable "endpoint_type" {
+  default = "Interface"
+}
+
+variable "private_dns_enabled" {
+  default= false
+}
+
+variable "subnet_ids" {
+  type = "list"
+}
+
+variable "name" {
+  
+}
+
+variable "security_group_ids" {
+  
+}
