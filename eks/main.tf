4ea8b3d18e73d448e6a9224ff766766c0078e252
Onur SALK <onursalk@kloia.com>
2020-02-24 12:27:03 +0000

    EKS Fixing cw log group

--- eks/main.tf
+++ eks/main.tf
@@ -3,7 +3,7 @@ data "aws_subnet_ids" "subnets" {
 }
 
 resource "aws_cloudwatch_log_group" "eks_log_group" {
-  count             = length(var.cluster_enabled_log_types)
+  count             = length(var.cluster_enabled_log_types) > 0 ? 1 : 0
   name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = var.cluster_log_retention_in_days
   kms_key_id        = var.cluster_log_kms_key_id
