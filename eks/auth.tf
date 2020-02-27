0dd1067e954dfb6652a7d6aa8ab7b96922fa9d63
Onur SALK <onursalk@kloia.com>
2020-02-27 09:54:47 +0000

    EKS Fixing auth and added harcoded k8s provider version

--- eks/auth.tf
+++ eks/auth.tf
@@ -3,10 +3,11 @@ data "aws_eks_cluster_auth" "cluster_auth" {
 }
 
 provider "kubernetes" {
-  host                   = aws_eks_cluster.eks.eks_cluster_endpoint
+  host                   = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster_auth.token
   load_config_file       = false
+  version                = "1.10.0" # Needs to be removed later (there is a bug with latest version)
 }
 
 data "template_file" "node_group_arns" {
