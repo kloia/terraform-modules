56470dd8c1d30d453e392884dc86394c86ef731c
Onur SALK <onursalk@kloia.com>
2020-03-02 15:45:42 +0000

    EKS Fix eks timeout issue by waiting endpoint

--- eks/auth.tf
+++ eks/auth.tf
@@ -2,6 +2,21 @@ data "aws_eks_cluster_auth" "cluster_auth" {
   name = var.cluster_name
 }
 
+
+resource "null_resource" "wait_for_eks" {
+
+  depends_on = [
+    aws_eks_cluster.eks
+  ]
+
+  provisioner "local-exec" {
+    command = "until wget --no-check-certificate -O - -q $ENDPOINT/healthz >/dev/null; do sleep 4; done"
+    environment = {
+      ENDPOINT = aws_eks_cluster.eks.endpoint
+    }
+  }
+}
+
 provider "kubernetes" {
   host                   = aws_eks_cluster.eks.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
@@ -21,6 +36,8 @@ data "template_file" "node_group_arns" {
 }
 
 resource "kubernetes_config_map" "aws_auth_configmap" {
+  depends_on = [null_resource.wait_for_eks]
+
   metadata {
     name      = "aws-auth"
     namespace = "kube-system"
