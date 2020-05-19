8578ba177873cb522850bcbfdc3f8e93406ebbc5
Emir Özbir <emirozbir@kloia.com>
2020-05-19 17:55:22 +0300

    output removed

--- elasticache/output.tf
+++
@@ -1,7 +0,0 @@
-output "cluster_id" {
-  value = "${aws_elasticache_replication_group.cluster.id}"
-}
-
-output "cluster_primary_endpoint" {
-  value = "${aws_elasticache_replication_group.cluster.primary_endpoint_address}"
-}
