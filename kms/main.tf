191e51ff019fbfd393fca45124bc9ae46a972abb
WoodProgrammer <emirozbir@kloia.com>
2020-09-23 13:20:13 +0300

    kms rotation option added

--- kms/main.tf
+++ kms/main.tf
@@ -7,13 +7,12 @@ resource "aws_kms_key" "kms_key" {
   deletion_window_in_days = "${var.deletion_window_in_days}"
   is_enabled = "${var.is_enabled}"
   enable_key_rotation = "${var.rotation}"
-  key_usage = "${var.key_usage}"
-  policy = "${var.policy}"
+  key_usage 	      = "${var.key_usage}"
+  policy 	      = "${var.policy}"
 }
-
 
 resource "aws_kms_alias" "kms_key_alias" {
   name          = "${var.alias_name}"
   target_key_id = "${aws_kms_key.kms_key.key_id}"
-  depends_on = ["aws_kms_key.kms_key"]
+  depends_on    = ["aws_kms_key.kms_key"]
 }
